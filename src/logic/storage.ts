91691df2a39b016696805f93ff7a7d9b2ecc3b2d
shiraishi <shiraishi_yuma@uniquevision.co.jp>
2022-07-23 13:08:07 +0900

    style: main page

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,3 +1,26 @@
+import { z } from 'zod'
 import { useStorageLocal } from '~/composables/useStorageLocal'
 
-export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo', { listenToStorageChanges: true })
+export const optionsSchema = z.object({
+  gitlabToken: z.string(),
+  clockifyToken: z.string(),
+  clockifyWorkspace: z.string(),
+})
+
+export async function getOptions() {
+  return optionsSchema.parse(
+    JSON.parse((await browser.storage.local.get('options')).options),
+  )
+}
+
+type Options = z.infer<typeof optionsSchema>
+
+export const storageOptions = useStorageLocal<Options>(
+  'options',
+  {
+    gitlabToken: '',
+    clockifyToken: '',
+    clockifyWorkspace: '',
+  },
+  { listenToStorageChanges: true },
+)
--- src/options/Options.vue
+++ src/options/Options.vue
@@ -1,19 +1,96 @@
 <script setup lang="ts">
-import { storageDemo } from '~/logic/storage'
+import { storageOptions } from '~/logic/storage'
 </script>
 
 <template>
-  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <img src="/assets/icon.svg" class="icon-btn mx-2 text-2xl" alt="extension icon">
-    <div>Options</div>
-    <p class="mt-2 opacity-50">
-      This is the options page
-    </p>
-
-    <input v-model="storageDemo" class="border border-gray-400 rounded px-2 py-1 mt-2">
-
-    <div class="mt-4">
-      Powered by Vite <pixelarticons-zap class="align-middle" />
+  <main class="p-5 flex flex-col items-center">
+    <div class="flex flex-col space-y-5" style="width: 40rem">
+      <div class="py-5 text-3xl">
+        <span class="text-sky-500">Clockify</span><span> to</span><span class="font-bold"> 日報</span>
+      </div>
+      <div class="flex flex-col space-y-3">
+        <div
+          class="p-5 rounded-lg bg-sky-100 drop-shadow-md flex flex-col space-y-3"
+        >
+          <div class="py-1 text-xl text-sky-500">
+            Settings
+          </div>
+          <div class="flex flex-row">
+            <div class="font-bold" style="width: 12rem">
+              Clockify Access Token
+            </div>
+            <input
+              v-model="storageOptions.clockifyToken"
+              class="flex-grow border border-sky-300 rounded px-1"
+              type="password"
+            >
+          </div>
+          <div class="flex flex-row">
+            <div class="font-bold" style="width: 12rem">
+              Clockify Workspace ID
+            </div>
+            <input
+              v-model="storageOptions.clockifyWorkspace"
+              class="flex-grow border border-sky-300 rounded px-1"
+            >
+          </div>
+          <div class="flex flex-row">
+            <div class="font-bold" style="width: 12rem">
+              集計日付
+            </div>
+            <div class="flex-grow">
+              <input
+                id="start"
+                type="date"
+                class="flex-grow border border-sky-300 rounded px-1"
+              >
+              <input
+                id="end"
+                type="date"
+                class="flex-grow border border-sky-300 rounded px-1"
+              >
+            </div>
+          </div>
+          <div class="flex flex-row">
+            <div class="font-bold" style="width: 12rem">
+              最小時間
+            </div>
+            <div class="flex-grow">
+              <input
+                id="min"
+                class="flex-grow border border-sky-300 rounded px-1"
+              >
+            </div>
+          </div>
+        </div>
+        <div class="flex flex-row justify-between space-x-3">
+          <button
+            class="flex-grow rounded bg-sky-500 text-white drop-shadow-md p-2"
+          >
+            指定期間で取得
+          </button>
+          <button
+            class="flex-grow rounded bg-sky-500 text-white drop-shadow-md p-2"
+          >
+            日報取得
+          </button>
+          <button
+            class="flex-grow rounded bg-sky-500 text-white drop-shadow-md p-2"
+          >
+            週報取得
+          </button>
+        </div>
+      </div>
+      <div
+        class="bg-sky-900 rounded-lg text-white text-sm text-sky-100 drop-shadow-md p-7 flex flex-col space-x-3"
+      >
+        <div
+          id="copy"
+          class="material-icons self-end rounded-full cursor-pointer p-2 hover:bg-sky-100 hover:text-sky-900"
+        >
+          content_copy
+        </div>
+      </div>
     </div>
   </main>
 </template>
