1d7c7619cb6fa2c5632b271829b5a0515d628da0
shiraishi <pharmacy.sy@gmail.com>
2023-05-08 17:21:39 +0900

    install: vuetify

--- src/options/main.ts
+++ src/options/main.ts
@@ -1,8 +1,20 @@
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import { createApp } from 'vue'
 import App from './Options.vue'
 import { setupApp } from '~/logic/common-setup'
 import '../styles'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
+app.use(vuetify)
 setupApp(app)
 app.mount('#app')
