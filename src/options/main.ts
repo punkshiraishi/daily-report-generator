fd738c9f6ee2aca9ee3fa96a621b93437a7cab0c
shiraishi <pharmacy.sy@gmail.com>
2023-05-12 10:31:43 +0900

    setup: vuetify icons

--- src/options/main.ts
+++ src/options/main.ts
@@ -3,6 +3,7 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 import { createApp } from 'vue'
 import App from './Options.vue'
@@ -12,6 +13,13 @@ import '../styles'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 const app = createApp(App)
